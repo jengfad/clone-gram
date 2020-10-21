569df0b3b49314aeb3ab9fee8219ec665f687d68
Jennifer Fadriquela <jenniferfadriquela@gmail.com>
2020-10-21 15:42:02 +0800

    switch to master

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/jengfad/clone-gram.git main:gh-pages
+git push -f https://github.com/jengfad/clone-gram.git master:gh-pages
 cd -
