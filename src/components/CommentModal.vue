5a7b91dd1d5e8b2cf437a3ae33fbafa7e8772133
Jennifer Fadriquela <jenniferfadriquela@gmail.com>
2020-10-21 18:36:42 +0800

    changed login layout

--- src/components/CommentModal.vue
+++ src/components/CommentModal.vue
@@ -2,7 +2,7 @@
   <div class="c-modal">
     <div class="c-container">
       <a @click="$emit('close')">close</a>
-      <p>add a comment</p>
+      <p>Add a comment:</p>
       <form @submit.prevent>
         <textarea v-model.trim="comment"></textarea>
         <button @click="addComment()" :disabled="comment == ''" class="button">add comment</button>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,54 +1,67 @@
 <template>
-  <div id="login">
+<div class="container h-100">
+  <div class="row h-100 justify-content-center align-items-center">
     <PasswordReset v-if="showPasswordReset" @close="togglePasswordReset()"></PasswordReset>
-    <section>
-      <div class="col1">
-        <h1>Clonegram</h1>
-        <p>Demo social media web app: Powered by Vue.js and Firebase.</p>
+    <div class="card">
+      <div class="card-body">
+        <div>
+          <h1>Clonegram</h1>
+          <p>Demo social media web app: Powered by Vue.js and Firebase.</p>
+        </div>
+        <div :class="{ 'signup-form': !showLoginForm }">
+          <form v-if="showLoginForm" @submit.prevent>
+            <h1>Welcome Back</h1>
+            <div class="d-flex flex-column">
+              <div>
+                <label for="email1">Email</label>
+                <input v-model.trim="loginForm.email" type="text" placeholder="you@email.com" id="email1" />
+              </div>
+              <div>
+                <label for="password1">Password</label>
+                <input v-model.trim="loginForm.password" type="password" placeholder="******" id="password1" />
+              </div>
+              <div class="align-self-end">
+                <button @click="login()" class="button">Log In</button>
+              </div>
+              <div class="d-flex pt-4 align-self-center">
+                <div>
+                  <a @click="togglePasswordReset()">Forgot Password</a>
+                </div>
+                <div class="px-2">|</div>
+                <div>
+                  <a @click="toggleForm()">Create an Account</a>
+                </div>
+              </div>
+            </div>
+          </form>
+          <form v-else @submit.prevent>
+            <h1>Get Started</h1>
+            <div>
+              <label for="name">Name</label>
+              <input v-model.trim="signupForm.name" type="text" placeholder="Jane Doe" id="name" />
+            </div>
+            <div>
+              <label for="title">Bio</label>
+              <input v-model.trim="signupForm.title" type="text" placeholder="What's your life passion?" id="title" />
+            </div>
+            <div>
+              <label for="email2">Email</label>
+              <input v-model.trim="signupForm.email" type="text" placeholder="you@email.com" id="email2" />
+            </div>
+            <div>
+              <label for="password2">Password</label>
+              <input v-model.trim="signupForm.password" type="password" placeholder="Mininum of 6 characters" id="password2" />
+            </div>
+            <button @click="signup()" class="button">Sign Up</button>
+            <div class="extras">
+              <a @click="toggleForm()">Back to Log In</a>
+            </div>
+          </form>
+        </div>
       </div>
-      <div :class="{ 'signup-form': !showLoginForm }" class="col2">
-        <form v-if="showLoginForm" @submit.prevent>
-          <h1>Welcome Back</h1>
-          <div>
-            <label for="email1">Email</label>
-            <input v-model.trim="loginForm.email" type="text" placeholder="you@email.com" id="email1" />
-          </div>
-          <div>
-            <label for="password1">Password</label>
-            <input v-model.trim="loginForm.password" type="password" placeholder="******" id="password1" />
-          </div>
-          <button @click="login()" class="button">Log In</button>
-          <div class="extras">
-            <a @click="togglePasswordReset()">Forgot Password</a>
-            <a @click="toggleForm()">Create an Account</a>
-          </div>
-        </form>
-        <form v-else @submit.prevent>
-          <h1>Get Started</h1>
-          <div>
-            <label for="name">Name</label>
-            <input v-model.trim="signupForm.name" type="text" placeholder="Jane Doe" id="name" />
-          </div>
-          <div>
-            <label for="title">Bio</label>
-            <input v-model.trim="signupForm.title" type="text" placeholder="What's your life passion?" id="title" />
-          </div>
-          <div>
-            <label for="email2">Email</label>
-            <input v-model.trim="signupForm.email" type="text" placeholder="you@email.com" id="email2" />
-          </div>
-          <div>
-            <label for="password2">Password</label>
-            <input v-model.trim="signupForm.password" type="password" placeholder="Mininum of 6 characters" id="password2" />
-          </div>
-          <button @click="signup()" class="button">Sign Up</button>
-          <div class="extras">
-            <a @click="toggleForm()">Back to Log In</a>
-          </div>
-        </form>
-      </div>
-    </section>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
